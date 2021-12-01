161647305106e6135c031564d1679d305d7648f3
MarineFlora <marine.rapeneau@gmail.com>
2021-12-01 18:14:20 +0100

    ajout foreign keys models post comment user and references migrations

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -3,13 +3,10 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
     class Comment extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
+        // Méthode pour définir les associations
         static associate(models) {
-        // define association here
+            Comment.belongsTo(models.User, { foreignKey: 'userId' }) 
+            Comment.belongsTo(models.Post, { foreignKey: 'postId' }) 
         }
     };
     Comment.init({
--- backend/models/post.js
+++ backend/models/post.js
@@ -3,13 +3,10 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
     class Post extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
+        // Méthode pour définir les associations
         static associate(models) {
-        // define association here
+            Post.belongsTo(models.User, { foreignKey: 'userId' })
+            Post.hasMany(models.Comment) 
         }
     };
     Post.init({
