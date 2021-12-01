ec058833f9084ff555479196dddc02b66d1c8608
MarineFlora <marine.rapeneau@gmail.com>
2021-12-01 10:40:11 +0100

    creation model, migration et table 'comments'

---
+++ backend/migrations/20211201093651-create-comment.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('Comments', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+        userId: {
+            type: Sequelize.INTEGER
+        },
+        postId: {
+            type: Sequelize.INTEGER
+        },
+        description: {
+            type: Sequelize.TEXT
+        },
+        imageUrl: {
+            type: Sequelize.STRING
+        },
+        likes: {
+            type: Sequelize.INTEGER
+        },
+        usersLiked: {
+            type: Sequelize.JSON
+        },
+        createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE
+        },
+        updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE
+        }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('Comments');
+    }
+};
