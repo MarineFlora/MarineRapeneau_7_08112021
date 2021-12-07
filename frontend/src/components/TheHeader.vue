3d82719bc88766a61681dcf9b13ee58136a7af8f
MarineFlora <marine.rapeneau@gmail.com>
2021-12-07 19:18:44 +0100

    ajout hover icons

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template> 
-    <header class="py-2 bg-dark">
+    <header class="py-2 bg-dark shadow">
         <b-container>
             <b-navbar>
 
@@ -7,7 +7,8 @@
                     <b-img 
                         height="45" 
                         :src="require('../assets/images/logo-white.svg')" 
-                        alt="Groupomania logo" 
+                        alt="Groupomania logo"
+                        title="accueil" 
                         class="d-md-none d-block"
                     ></b-img>
                     <b-img 
@@ -19,14 +20,45 @@
                 </b-navbar-brand>
 
                 <b-navbar-nav class="ml-auto align-items-center">
-                    <b-nav-item to="/user-profile">
-                        <b-icon icon="person" font-scale="2.2" class="text-white"></b-icon>
+                    <b-nav-item to="/user-profile" @mouseover="hoverProfile = true" @mouseleave="hoverProfile = false">
+                        <b-icon 
+                            v-if="!hoverProfile" 
+                            icon="person" 
+                            font-scale="2.2" 
+                            class="text-white"
+                        ></b-icon>
+                        <b-icon 
+                            v-if="hoverProfile" 
+                            icon="person-fill" 
+                            variant="primary" 
+                            font-scale="2.2" 
+                            title="profil"
+                        ></b-icon>
                     </b-nav-item>
-                    <b-nav-item to="/about">
-                        <b-icon icon="info-circle" font-scale="1.8" class="text-white"></b-icon>
+
+                    <b-nav-item to="/about" @mouseover="hoverInfo = true" @mouseleave="hoverInfo = false">
+                        <b-icon 
+                            v-if="!hoverInfo" 
+                            icon="info-circle" 
+                            font-scale="1.8" 
+                            class="text-white"
+                        ></b-icon>
+                        <b-icon 
+                            v-if="hoverInfo" 
+                            icon="info-circle-fill" 
+                            variant="primary" 
+                            font-scale="1.8" 
+                            title="à propos"
+                        ></b-icon>
                     </b-nav-item>
+
                     <b-nav-item to="/login">
-                        <b-icon icon="power" font-scale="2" class="text-white"></b-icon>
+                        <b-icon 
+                            icon="power" 
+                            font-scale="2" 
+                            class="logout-icon"
+                            title="se deconnecter"
+                        ></b-icon>
                     </b-nav-item>
                 </b-navbar-nav>
 
@@ -38,9 +70,12 @@
 <script> 
     export default {
         name: 'TheHeader',
-        props: {
-            
-        }
+        data() {
+            return {
+                hoverProfile: false,
+                hoverInfo: false, 
+            };
+        }        
     }
 </script>
 
@@ -48,4 +83,12 @@
 header {
     border-radius: 0 0 0.8rem 0.8rem;
 }
+
+.logout-icon {
+    color: #fff;
+    &:hover {
+        color: #d1515a;
+    }
+}
+
 </style>
